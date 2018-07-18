306d962dd20efc26d80d4dc7220fec5a88531475
Raphael R Costa <rxonda@gmail.com>
2018-07-17 23:41:16 -0300

    Changing start task to point to heroku config

--- config/bs-heroku-config.js
+++ config/bs-heroku-config.js
@@ -1,6 +1,6 @@
 var proxy = require('http-proxy-middleware');
 var options = {
-  target: "http://stockreader.herokuapp.com",
+  target: "https://stockreader.herokuapp.com",
   changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
   logLevel: 'debug',
   pathRewrite: {'^/api' : ''}
@@ -8,7 +8,7 @@ var options = {
 var apiProxy = proxy('/api', options);
 module.exports = function(bs){
   return {
-    port: process.env.PORT,
+    port: (process.env.PORT || 8090),
     files: ["./**/*.{html,htm,css,js}"],
     server: {
       baseDir: "./",
