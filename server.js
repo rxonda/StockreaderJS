d93fcecfd6c17cf440498ed4cf17c881ed2c842f
Raphael R Costa <rxonda@gmail.com>
2018-07-24 09:20:23 -0300

    Fixing server

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var proxy = require('http-proxy-middleware');
 
 var options = {
-  target: "http://stockreader.herokuapp.com",
-  changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
-  logLevel: 'debug',
-  pathRewrite: {"^/api" : ""}
+
+    target: "http://stockreader.herokuapp.com",
+  //  target: "http://localhost:8080",
+    changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
+    logLevel: 'debug',
+    pathRewrite: {"^/api" : ""}
 };
 
 var app = express();
@@ -16,9 +18,12 @@ app.use(express.static(__dirname + '/dist'));
 
 app.use('/api', apiProxy);
 
-// app.use('/', function(req, res) {
-//     res.render('index');
-// });
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', function(req, res) {
+    res.render('index.html');
+});
 
 var port = process.env.PORT || 3000;
 
