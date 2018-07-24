abb4066a462f01df39ae525dd8c391cf8355a982
Raphael R Costa <rxonda@gmail.com>
2018-07-24 09:06:37 -0300

    Not using any rendering engine

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static(__dirname + '/dist'));
 
 app.use('/api', apiProxy);
 
-app.use('/', function(req, res) {
-    res.render('index');
-});
+// app.use('/', function(req, res) {
+//     res.render('index');
+// });
 
 var port = process.env.PORT || 3000;
 
